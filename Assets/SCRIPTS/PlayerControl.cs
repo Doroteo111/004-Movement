dc3f2149d80fef2b2bdb47137de65e8a4c27cfb7
Doroteo111 <ladoritaesta@gmail.com>
2022-11-15 12:19:12 +0100

    proyecto acabado

--- Assets/SCRIPTS/PlayerControl.cs
+++ Assets/SCRIPTS/PlayerControl.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    public float speed = 20f;
-    public float turnSpeed = 10f;
-    public float horizontalInput;
+    private float speed = 20f;
+    private float turnSpeed = 40f;
+    public float horizontalInput, verticalInput;  //CÓDIGO LIMPIO 
+
+     
+
     void Update()
     {
 
         //detectar el input hoeizontal
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         //Movimiento pa'lante
         //transform.Translate(0, 0, 1); 
-        transform.Translate(Vector3.forward * 20 * Time.deltaTime);
+        transform.Translate(Vector3.forward * 20 * Time.deltaTime * verticalInput);
 
         //Movimiento lateral manual
-        transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
+        /*transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);*/
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
 
     }
 }
